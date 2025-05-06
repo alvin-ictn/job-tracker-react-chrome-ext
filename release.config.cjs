fc2805dfb8b7b92374f4f7b29ac0aeb8b9a0ad1e
Alvin Mantovani <alvin.ictn@gmail.com>
2025-05-06 15:36:18 +0700

    chore: fix sync persion configuration

--- release.config.cjs
+++ release.config.cjs
@@ -1,13 +1,38 @@
 module.exports = {
-    branches: ["master"],
-    plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/changelog",
-      "@semantic-release/npm",
-      "@semantic-release/git",
-      "@semantic-release/github"
+  branches: ["main", "master"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+
+    "@semantic-release/npm",
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "npm run build && npm run package"
+      }
     ],
-    preset: "conventionalcommits",
-  };
-  
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          "job-tracker-extension.zip",
+          "job-tracker-extension.crx"
+        ]
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: [
+          "package.json",
+          "manifest.json",
+          "CHANGELOG.md"
+        ],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
+  ],
+  preset: "conventionalcommits"
+};
